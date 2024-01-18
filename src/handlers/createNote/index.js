142774be45c704686cac425f92eb54918cbec7ba
Debora Martinez <deboraaspesimartinez@gmail.com>
2024-01-18 15:19:45 -0300

    add joi to create note and data.json to invoke locally

--- src/handlers/createNote/index.js
+++ src/handlers/createNote/index.js
@@ -1,18 +1,30 @@
 const dynamoDBService = require("../../../service/aws/dynamoDb");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
+const Joi = require("joi");
 
+const noteSchema = Joi.object({
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+});
 
 exports.handler = async (event) => {
-
   const data = JSON.parse(event.body);
 
+  const { error } = noteSchema.validate(data);
+  if (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Invalid data", details: error.details }),
+    };
+  }
+
   try {
-    const data = {
+    const database = {
       id: uuidv4(),
       title: data.title,
       content: data.content,
     };
-    await dynamoDBService.createItem(data, process.env.NOTES_TABLE);
+    await dynamoDBService.createItem(database, process.env.NOTES_TABLE);
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Note created" }),
