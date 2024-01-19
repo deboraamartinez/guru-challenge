5cab990f956011b58ba596cd0d12bb340bdeaa7c
Debora Martinez <deboraaspesimartinez@gmail.com>
2024-01-18 23:22:40 -0300

    fix data from readNote

--- src/handlers/readNote/index.js
+++ src/handlers/readNote/index.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
   const id = event.pathParameters.id;
 
   try {
-    const data = await dynamoDBService.readItem('5abd3242-688c-4a87-baf1-c1c75ec57356', process.env.NOTES_TABLE);
+    const data = await dynamoDBService.readItem(id, process.env.NOTES_TABLE);
     console.log(data)
     return {
       statusCode: 200,
